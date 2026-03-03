9a109ba4820bb4e66fae7e6d33a89895bdc962da
Callum Macpherson <callum.macpherson@quantinuum.com>
2026-03-03 11:41:59 +0000

    cleanup

--- tket-qsystem/src/lib.rs
+++ tket-qsystem/src/lib.rs
@@ -175,15 +175,14 @@ impl QSystemPass {
         // Squash single qubit gates after conversion to the Qsystem gate set.
         // Call the SquashRzPhasedX pass from pytket using the pass JSON
         // https://docs.quantinuum.com/tket/api-docs/passes.html#pytket.passes.SquashRzPhasedX
-        let squas_pass = serde_json::to_string(&tket_json_rs::pass::BasePass::StandardPass { pass: tket_json_rs::pass::standard::StandardPass::SquashRzPhasedX }).unwrap();
-
+        let squash_pass = serde_json::to_string(&tket_json_rs::pass::BasePass::StandardPass { pass: tket_json_rs::pass::standard::StandardPass::SquashRzPhasedX }).unwrap();
         let circ = Circuit::new(hugr);
         let mut encoded = EncodedCircuit::new(circ.hugr(), EncodeOptions::new().with_subcircuits(true)).unwrap();
          encoded
         .par_iter_mut()
         .for_each(|(_region, serial_circuit)| {
             let mut circuit_ptr = Tket1Circuit::from_serial_circuit(serial_circuit).unwrap();
-            Tket1Pass::run_from_json(&squas_pass, &mut circuit_ptr).unwrap();
+            Tket1Pass::run_from_json(&squash_pass, &mut circuit_ptr).unwrap();
             *serial_circuit = circuit_ptr.to_serial_circuit().unwrap();
 
         });
@@ -387,8 +386,6 @@ mod test {
             hugr.set_entrypoint(main_node);
 
         }
-        //println!("Yo{}", hugr.mermaid_string()); 
-        //assert!(0 == 1);   
         QSystemPass::default().run(&mut hugr).unwrap();
 
         let topo_sorted = Topo::new(&hugr.as_petgraph())
