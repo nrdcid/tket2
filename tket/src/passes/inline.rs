51a4e89bfdcc06854efc77577152c77b49129085
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2026-03-16 21:46:33 +0000

    tests

--- tket/src/passes/inline.rs
+++ tket/src/passes/inline.rs
@@ -31,7 +31,7 @@ impl Metadata for InlineAnnotation {
 }
 
 /// Errors that may be raised by [InlinePass]
-#[derive(Clone, Debug, derive_more::Display, derive_more::Error)]
+#[derive(Clone, Debug, PartialEq, Eq, derive_more::Display, derive_more::Error)]
 pub enum InlineError<N = Node> {
     /// Functions annotated with [InlineAnnotation::Always] form a cycle
     /// so inlining would produce an infinitely-big program
@@ -41,6 +41,7 @@ pub enum InlineError<N = Node> {
 
 /// A [ComposablePass] that inlines [Call]s to functions
 /// according to [InlineAnnotation]s.
+#[derive(Default, Clone, Debug)]
 pub struct InlinePass {
     scope: PassScope,
 }
@@ -142,6 +143,122 @@ fn do_inline<H: HugrMut>(call: H::Node, hugr: &mut H) {
         }
         Err(e) => {
             todo!("Update to handle {e:?}")
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use rstest::rstest;
+    use std::collections::HashSet;
+
+    use hugr::{
+        HugrView,
+        builder::{
+            Container, Dataflow, DataflowHugr, DataflowSubContainer, FunctionBuilder, HugrBuilder,
+        },
+        extension::prelude::{qb_t, usize_t},
+        hugr::hugrmut::HugrMut,
+        ops::handle::NodeHandle,
+        types::Signature,
+    };
+    use hugr_passes::{ComposablePass, RemoveDeadFuncsPass, inline_dfgs::InlineDFGsPass};
+
+    use super::{InlineAnnotation, InlineError, InlinePass};
+
+    #[test]
+    fn test_single_cycle() {
+        let mut main = FunctionBuilder::new("main", Signature::new_endo([qb_t(), qb_t()])).unwrap();
+        let mut mb = main.module_root_builder();
+        let mut fb = mb
+            .define_function("self-call", Signature::new_endo([qb_t()]))
+            .unwrap();
+        let c = fb
+            .call::<true>(&fb.container_node().into(), &[], fb.input_wires())
+            .unwrap();
+        let fb = fb.finish_with_outputs(c.outputs()).unwrap();
+        let inputs = main.input_wires();
+        let mut hugr = main.finish_hugr_with_outputs(inputs).unwrap();
+        hugr.set_metadata::<InlineAnnotation>(fb.node(), InlineAnnotation::Always);
+        let backup = hugr.clone();
+
+        // We error even though the function is not called
+        let e = InlinePass::default().run(&mut hugr).unwrap_err();
+        assert_eq!(e, InlineError::AlwaysCycle(vec![fb.node()]));
+        assert_eq!(hugr, backup);
+
+        RemoveDeadFuncsPass::default().run(&mut hugr).unwrap();
+        assert_eq!(
+            hugr.children(hugr.module_root()).collect::<Vec<_>>(),
+            [hugr.entrypoint()]
+        );
+        let backup = hugr.clone();
+        InlinePass::default().run(&mut hugr).unwrap();
+        assert_eq!(hugr, backup);
+    }
+
+    #[test]
+    fn cycle() {
+        let mut main = FunctionBuilder::new("main", Signature::new_endo([usize_t()])).unwrap();
+        let main_h = main.container_node().into();
+        let mut mb = main.module_root_builder();
+        let mut fb1 = mb
+            .define_function("f1", Signature::new_endo([usize_t()]))
+            .unwrap();
+        let c1 = fb1.call::<true>(&main_h, &[], fb1.input_wires()).unwrap();
+        let fb1 = fb1.finish_with_outputs(c1.outputs()).unwrap();
+        let c2 = main.call(fb1.handle(), &[], main.input_wires()).unwrap();
+        let mut hugr = main.finish_hugr_with_outputs(c2.outputs()).unwrap();
+        hugr.set_metadata::<InlineAnnotation>(hugr.entrypoint(), InlineAnnotation::Always);
+        InlinePass::default().run(&mut hugr.clone()).unwrap(); // Ok
+
+        hugr.set_metadata::<InlineAnnotation>(fb1.node(), InlineAnnotation::Always);
+        let e = InlinePass::default().run(&mut hugr).unwrap_err();
+        assert_eq!(
+            e,
+            InlineError::AlwaysCycle(vec![fb1.node(), hugr.entrypoint()])
+        );
+    }
+
+    #[rstest]
+    fn test_one_deep(#[values(1, 2, 5)] num_calls: usize, #[values(false, true)] always: bool) {
+        let mut main =
+            FunctionBuilder::new("main", Signature::new_endo([qb_t(), qb_t(), qb_t()])).unwrap();
+
+        let mut mb = main.module_root_builder();
+        let swap = mb
+            .define_function("called-once", Signature::new_endo([qb_t(), qb_t()]))
+            .unwrap();
+        let [a, b] = swap.input_wires_arr();
+        let swap = swap.finish_with_outputs([b, a]).unwrap();
+
+        let [mut a, mut b, c] = main.input_wires_arr();
+        for _ in 0..num_calls {
+            [a, b] = main.call(swap.handle(), &[], [a, b]).unwrap().outputs_arr();
+        }
+        let mut hugr = main.finish_hugr_with_outputs([a, b, c]).unwrap();
+        if always {
+            hugr.set_metadata::<InlineAnnotation>(swap.node(), InlineAnnotation::Always);
+        }
+        let backup = hugr.clone();
+
+        InlinePass::default().run(&mut hugr).unwrap();
+        hugr.validate().unwrap();
+        let inlined = always || num_calls == 1;
+        assert_eq!(
+            hugr.static_targets(swap.node()).unwrap().count(),
+            if inlined { 0 } else { num_calls }
+        );
+        if inlined {
+            assert_eq!(hugr, backup);
+        } else {
+            InlineDFGsPass::default().run(&mut hugr).unwrap();
+            hugr.validate().unwrap();
+            let [inp, outp] = hugr.get_io(hugr.entrypoint()).unwrap();
+            assert_eq!(
+                HashSet::from_iter(hugr.input_neighbours(outp)),
+                HashSet::from([inp])
+            );
         }
     }
 }
