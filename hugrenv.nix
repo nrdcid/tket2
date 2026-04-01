fc26629d4ebaa7138f6c69a7de182b05c9ba1518
Jake Arkinstall <65358059+jake-arkinstall@users.noreply.github.com>
2026-04-01 21:41:31 +0100

    Bump to hugrenv 0.3.1 (GMP removal)

--- hugrenv.nix
+++ hugrenv.nix
@@ -1,16 +1,16 @@
 { pkgs }:
 let
-  hugrenvVersion = "0.3.0";
+  hugrenvVersion = "0.3.1";
   currentPlatform = if pkgs.stdenv.isDarwin then "macosx_15_0" else "manylinux_2_28";
   currentArch = if pkgs.stdenv.isAarch64 then "aarch64" else "x86_64";
   currentPlatformArch = "${currentPlatform}_${currentArch}";
 
   hugrenvUrl = "https://github.com/Quantinuum/hugrverse-env/releases/download/v${hugrenvVersion}/hugrenv-tket-${currentPlatformArch}.tar.gz";
   hugrenvHashes = {
-    "macosx_15_0_aarch64"    = "sha256-IrapjnU2H3Eb06sxXr9M8KpL0qjBwqot9QUudBBJYvA=";
-    "macosx_15_0_x86_64"     = "sha256-9Tyrsrxz8/LMR/q2jwmdTO77tgk1NCUFrc5/ilCjgtM=";
-    "manylinux_2_28_aarch64" = "sha256-OdHJj3pnxdet5dpEV4aVvom9pb6lZ1koyyJX0vHVns8=";
-    "manylinux_2_28_x86_64"  = "sha256-YAZ1kQK4MU3rrgXiyopTeSrx4cBhVEAxJyxoB5DIDqM=";
+    "macosx_15_0_aarch64"    = "sha256-4CZUc9GWnhAej8lMgCFMNiUb98EUzQOklWGQ30ENQ4k=";
+    "macosx_15_0_x86_64"     = "sha256-tYEjqF0s/30G8WZTJ0UPYpcnFM6pQOHFH7giUHScRaw=";
+    "manylinux_2_28_aarch64" = "sha256-NFcgtBxx4CURQYQjrR4VMn6LCLkT99OJqnRP1ilil0k=";
+    "manylinux_2_28_x86_64"  = "sha256-cj2kx3mCgXe5km9n6MI56S8IySQMX9ZcMsZ3a8T0kh0=";
   };
 in pkgs.fetchzip {
   url = hugrenvUrl;
